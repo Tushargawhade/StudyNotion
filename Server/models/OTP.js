048f189fe1236358fc2adfc38d9968501d7fa668
Tusshar <tushargawhade7@gmail.com>
2026-02-03 00:53:19 +0530

    verification mail added

--- Server/models/OTP.js
+++ Server/models/OTP.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require('mongoose');
 const monogoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const otpSchema = new monogoose.Schema({
 
@@ -20,5 +21,29 @@ const otpSchema = new monogoose.Schema({
     }
     
 })
+
+
+// async function to send mail
+
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(email, "Verification mail from studynotion",otp);
+        console.log("Email sent Successufully",mailResponse);
+
+    }
+    catch(error){
+        console.log("Error occure in sending mail",error)
+        throw error;
+    }
+
+}
+
+otpSchema.pre("save",async function(next) {
+    await sendVerificationEmail(this.email, this.otp)
+    next();
+    
+})
+
+
 
 module.exports = mongoose.model("OTP",otpSchema)
