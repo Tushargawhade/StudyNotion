049ea6bf37ade85d3742b2cc1ba580620db839a9
Tusshar <tushargawhade7@gmail.com>
2026-02-07 20:10:11 +0530

    send mail

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -10,7 +10,6 @@ const mailSender  = async (email, title, body)=>{
             auth:{
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS
-
             }
         })
 
@@ -22,7 +21,6 @@ const mailSender  = async (email, title, body)=>{
         })
 
         console.log(info);
-
         return info;
 
     }
