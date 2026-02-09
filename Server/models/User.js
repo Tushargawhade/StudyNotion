c6f324e6a763904b1dbe5239993f054dbc87eec9
Tusshar <tushargawhade7@gmail.com>
2026-02-09 23:43:16 +0530

    change

--- Server/models/User.js
+++ Server/models/User.js
@@ -47,6 +47,14 @@ const userSchema = new mongoose.Schema({
         required:true,
     },
 
+    token : {
+        type : String,
+    },
+
+    resetPasswordExpires:{
+        type : Date,
+    },
+
     cousesprogress:{
         type: monogoose.Schema.Types.ObjectId,
         required:true,
