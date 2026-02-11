3b18d16eece4190388de42f0b576e14248b1d824
Tusshar <tushargawhade7@gmail.com>
2026-02-12 02:15:48 +0530

    course controller

---
+++ Server/controllers/Course.js
@@ -0,0 +1,154 @@
+const { response } = require('express');
+const Course = require('../models/Course');
+const Tag = require('../models/Tags');
+const User = require('../models/User');
+
+const {uploadImageToCloudinary} = require('../utils/imageUploader')
+
+// Create course handler function  
+
+exports.createCourse = async (req,res)=>{
+
+    try{
+
+        // fetch data 
+        const {courseName, courseDescription, whatWillYouLearn, price, tag } = req.body;
+
+        // fetch thumbnail
+        const thumbnail = req.files.thumbnaliImage;
+
+        // validation 
+        if(!courseName || !courseDescription || !whatWillYouLearn || !price || !tag || !thumbnail){
+            return res.status(400).json({
+                success : false,
+                message : "All fields are requires for course creation "
+            })
+        }
+
+        // Changes Required in userId  and InstructorDetails   
+
+
+        // check for instructor 
+        
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor ID : ",instructorDetails);
+
+        if(!instructorDetails){
+            return res.status(404).json({
+                success : false,
+                message : "Instructor detail not found"
+            })
+        };
+
+
+        // check given tag is valid or not  
+        const tagDetails  = await Tag.findById(tag);
+        if(!tagDetails){
+            return res.status(404).json({
+                success : false,
+                message : "Tag detail not found"
+            })
+        };
+
+
+
+
+        // upload image to cloudinary 
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+
+        // create an entry for new course 
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor : instructorDetails._id,
+            whatWillYouLearn : whatWillYouLearn,
+            price,
+            tag : tagDetails._id,
+            thumbnail :thumbnailImage.secure_url
+        })
+
+
+        // add the new course to the user schema of instructor 
+
+        await User.findByIdAndUpdate(
+                    {_id : instructorDetails._id},
+                    {
+                        $push :{
+                            course :newCourse._id,
+                        }
+                    },
+                    {new : true}
+                )
+
+
+        // update the tag schema 
+
+
+
+
+        // return response  
+        return res.status(200).json({
+            success : true,
+            message : "Course Created Successfully",
+            data : newCourse,
+        })
+
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            errormessage : "Error in creating Course",
+            message :error.message,
+            
+        })
+    }
+
+
+
+}
+
+
+
+// Get All course handler function  
+exports.showAllCourses = async (req,res)=>{
+
+    try{
+
+        const allCourses = await Course.find({},
+                                                {courseName : true,
+                                                 price : true,
+                                                 thumbnail :true,
+                                                 instructor : true,
+                                                 ratingAndreview :true,
+                                                 studentEnrolled : true},)
+                                                .populate("Instructor")
+                                                .exec();
+
+        return res.status(200).json({
+            success: true,
+            message : "Data for all courses fetched successfully",
+            allCourses, 
+        })
+                                                    
+
+        
+
+
+
+
+
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message : "Cannot fetch the all courses!!"
+        })
+    }
+
+
+
+}
