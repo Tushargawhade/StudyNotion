8dd9bb2259da1292ad4886023faf905a3e1ad7c1
Tusshar <tushargawhade7@gmail.com>
2026-02-20 21:52:53 +0530

    update

--- Server/utils/imageUploader.js
+++ Server/utils/imageUploader.js
@@ -2,6 +2,7 @@ const cloudinary = require('cloudinary').v2
 
 exports.uploadImageToCloudinary = async(file, folder, height, quality)=>{
 
+   try{
     const options = {folder};
 
     if(height){
@@ -14,5 +15,14 @@ exports.uploadImageToCloudinary = async(file, folder, height, quality)=>{
     options.resource_type = 'auto';
 
     return await cloudinary.uploader.upload(file.tempFilePath, options);
+
+   }
+   catch(error){
+    return res.status(500).json({
+      success:false,
+      message:"Error while uploading image, please try again"
+    })
+
+   }
     
 }
