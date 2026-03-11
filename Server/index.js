cf35458d8e0481ded9100c7fe7ccbe8b7f0a7743
Tusshar <tushargawhade7@gmail.com>
2026-03-12 01:51:37 +0530

    initial frontend

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // database connectin 
 connect();
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="bg-richblack-900 text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-50">
+        Study Notion
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+  
