a171e3626144b4c6586bbe55fa2fbe86538c726e
Tusshar <tushargawhade7@gmail.com>
2026-03-16 14:58:52 +0530

    added

---
+++ src/components/core/homepage/InstructorSection.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Instructor from "../../../assets/Images/Instructor.png"
+import HighlightedText from './HighlightedText'
+import CTAButton from './Button'
+import { FaArrowRight } from "react-icons/fa6";
+
+const InstructorSection = () => {
+  return (
+    <div className='mt-16'>
+
+        <div className='flex gap-20 items-center'>
+
+            <div className='w-[50%] '>
+                <img src={Instructor} alt="Instructor" className='shadow-white' />
+            </div>
+
+            <div className='w-[50%] flex flex-col gap-10 '>
+                
+                <div className='text-4xl font-semibold w-[50%]'>
+                    Become an 
+                    <HighlightedText text={"Instructor"} />
+                </div>
+
+                <p className='text-richblack-300 text-[16px] font-medium w-[80%]'>
+                    Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.
+                </p>
+
+                <div className='w-fit'>
+
+                    <CTAButton active={true} linkto={"/signup"}>
+                        <div className='flex gap-2'>
+                            Start Learning Today 
+                            <FaArrowRight />
+                        </div>
+                    </CTAButton>
+
+                </div>
+                
+
+            </div>
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default InstructorSection
