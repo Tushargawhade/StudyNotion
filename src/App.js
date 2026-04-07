bb1df2471954f5fbf4862ced282dcace520e910a
Tusshar <tushargawhade7@gmail.com>
2026-04-07 21:26:02 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import "./App.css"
-import { Route,Routes } from "react-router-dom";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
-    
+
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+
+      <Navbar/>
+
       <Routes>
-
-        <Route path="/" element={<Home/>} />
-
-
+        <Route path="/" element={<Home />} />
       </Routes>
-
-
     </div>
-    
+
   );
 }
 
 export default App;
-  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Toaster } from "react-hot-toast";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from './reducer/index'
+import {Toaster} from "react-hot-toast"
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    
-      <App />
-
-    </BrowserRouter>    
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
+
